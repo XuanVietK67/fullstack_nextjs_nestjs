2e9ea1eff61b1f2a06fa157ebaae88f7ff79e2c5
Xuan Viet <phamxuanviet14920041@gmail.com>
2024-09-19 09:10:56 +0700

    config mailerModule and test sent email success

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,10 +4,13 @@ import { JwtAuthGuard } from '@/auth/passport/jwt-auth.guard';
 import { LocalAuthGuard } from '@/auth/passport/local-auth.guard';
 import { Public } from '@/auth/decoration/customizePublicAccessToken';
 import { CreateRegisterUserDto } from '@/auth/schemas/create-auth.dto';
-
+import { MailerService } from '@nestjs-modules/mailer';
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+    constructor(
+        private authService: AuthService,
+        private readonly mailerService: MailerService,
+    ) { }
     @UseGuards(LocalAuthGuard)
     @Public()
     @Post('login')
@@ -16,12 +19,24 @@ export class AuthController {
     }
     @UseGuards(JwtAuthGuard)
     @Get('profile')
-    getProfile(@Request() req) { 
+    getProfile(@Request() req) {
         return req.user;
     }
     @Post('register')
     @Public()
-    postRegister(@Body() registerDto: CreateRegisterUserDto){
+    postRegister(@Body() registerDto: CreateRegisterUserDto) {
         return this.authService.register(registerDto)
+    }
+    @Get('mail')
+    @Public()
+    TestMail() {
+        this.mailerService
+            .sendMail({
+                to: 'xuanvietdev@gmail.com', // list of receivers
+                subject: 'Testing Nest MailerModule ✔', // Subject line
+                text: 'welcome', // plaintext body
+                html: '<b>hello world from xuanvietdev </b>', // HTML body content
+            })
+        return "sent email success"
     }
 }
